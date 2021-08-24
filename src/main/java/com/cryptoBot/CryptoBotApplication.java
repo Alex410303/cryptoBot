e7b1584c740f10bed3c6a94b7c14c80eaa6cdcc3
admin <alex410303@gmail.com>
2021-08-24 18:42:06 +0300

    add maven

--- src/main/java/com/cryptoBot/CryptoBotApplication.java
+++ src/main/java/com/cryptoBot/CryptoBotApplication.java
@@ -8,6 +8,7 @@ public class CryptoBotApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CryptoBotApplication.class, args);
+		System.out.println("Helo world");
 	}
 
 }
