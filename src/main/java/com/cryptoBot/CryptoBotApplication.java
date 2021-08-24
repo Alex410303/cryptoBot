d65d81a7d35e153b7d60e8520b718eff31335d9e
admin <alex410303@gmail.com>
2021-08-24 17:30:06 +0300

    add maven

---
+++ src/main/java/com/cryptoBot/CryptoBotApplication.java
@@ -0,0 +1,13 @@
+package com.cryptoBot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoBotApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cryptoBot/CryptoBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cryptoBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CryptoBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
