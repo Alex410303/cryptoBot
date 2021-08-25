f3a83ff6258a4a640cb800329683de2bbaa093e7
admin <alex410303@gmail.com>
2021-08-25 22:04:15 +0300

    add bot

--- src/main/java/com/cryptoBot/CryptoBotApplication.java
+++ src/main/java/com/cryptoBot/CryptoBotApplication.java
@@ -1,7 +1,11 @@
 package com.cryptoBot;
 
+import com.cryptoBot.bots.ChatBot;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.telegram.telegrambots.meta.TelegramBotsApi;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.updatesreceivers.DefaultBotSession;
 
 @SpringBootApplication
 public class CryptoBotApplication {
@@ -9,6 +13,13 @@ public class CryptoBotApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(CryptoBotApplication.class, args);
 		System.out.println("Helo world");
+		try {
+			TelegramBotsApi telegramBotsApi = new TelegramBotsApi(DefaultBotSession.class);
+			telegramBotsApi.registerBot(new ChatBot());
+		} catch (TelegramApiException e) {
+			e.printStackTrace();
+		}
+
 	}
 
 }
---
+++ src/main/java/com/cryptoBot/bots/ChatBot.java
@@ -0,0 +1,33 @@
+package com.cryptoBot.bots;
+
+import org.telegram.telegrambots.bots.TelegramLongPollingBot;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
+import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+
+public class ChatBot extends TelegramLongPollingBot {
+    @Override
+    public String getBotUsername() {
+        return "ChatBot";
+    }
+
+    @Override
+    public String getBotToken() {
+        return "setBotToken!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!";
+    }
+
+    @Override
+    public void onUpdateReceived(Update update) {
+        if (update.hasMessage() && update.getMessage().hasText()) {
+            SendMessage message = new SendMessage(); // Create a SendMessage object with mandatory fields
+            message.setChatId(update.getMessage().getChatId().toString());
+            message.setText(update.getMessage().getText());
+
+            try {
+                execute(message); // Call method to send the message
+            } catch (TelegramApiException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
